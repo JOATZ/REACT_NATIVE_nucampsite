a44551d67d7361f5e6c2aefb2a1bea44845e7e5d
JOATZ <136692977+JOATZ@users.noreply.github.com>
2023-11-27 12:32:12 +0200

    Stack Nav Icons

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -1,4 +1,4 @@
-import { Platform, View } from 'react-native'
+import { Platform, StyleSheet, View } from 'react-native'
 import Constants from 'expo-constants'
 import { createStackNavigator } from '@react-navigation/stack'
 import { createDrawerNavigator } from '@react-navigation/drawer'
@@ -8,6 +8,7 @@ import DirectoryScreen from './DirectoryScreen'
 import CampsiteInfoScreen from './CampsiteInfoScreen'
 import AboutScreen from './AboutScreen'
 import ContactScreen from './ContactScreen'
+import { Icon } from 'react-native-elements'
 
 const Drawer = createDrawerNavigator()
 
@@ -20,7 +21,21 @@ const HomeNavigator = () => {
     const Stack = createStackNavigator()
     return (
         <Stack.Navigator screenOptions={screenOptions}>
-            <Stack.Screen name='Home' component={HomeScreen} />
+            <Stack.Screen
+                name='Home'
+                component={HomeScreen}
+                options={({ navigation }) => ({
+                    title: 'Home',
+                    headerLeft: () => (
+                        <Icon
+                            name='home'
+                            type='font-awesome'
+                            iconStyle={styles.stackIcon}
+                            onPress={() => navigation.toggleDrawer()}
+                        />
+                    )
+                })}
+            />
         </Stack.Navigator>
     )
 }
@@ -29,7 +44,20 @@ const AboutNavigator = () => {
     const Stack = createStackNavigator()
     return (
         <Stack.Navigator screenOptions={screenOptions}>
-            <Stack.Screen name='About' component={AboutScreen} />
+            <Stack.Screen
+                name='About'
+                component={AboutScreen}
+                options={({ navigation }) => ({
+                    headerLeft: () => (
+                        <Icon
+                            name='info-circle'
+                            type='font-awesome'
+                            iconStyle={styles.stackIcon}
+                            onPress={() => navigation.toggleDrawer()}
+                        />
+                    )
+                })}
+            />
         </Stack.Navigator>
     )
 }
@@ -41,7 +69,17 @@ const ContactNavigator = () => {
             <Stack.Screen
                 name='Contact'
                 component={ContactScreen}
-                options={{ title: 'Contact Us' }}
+                options={({ navigation }) => ({
+                    title: 'Contact Us',
+                    headerLeft: () => (
+                        <Icon
+                            name='address-card'
+                            type='font-awesome'
+                            iconStyle={styles.stackIcon}
+                            onPress={() => navigation.toggleDrawer()}
+                        />
+                    )
+                })}
             />
         </Stack.Navigator>
     )
@@ -57,7 +95,17 @@ const DirectoryNavigator = () => {
             <Stack.Screen
                 name='Directory'
                 component={DirectoryScreen}
-                options={{ title: 'Campsite Directory' }}
+                options={({ navigation }) => ({
+                    title: 'Campsite Directory',
+                    headerLeft: () => (
+                        <Icon
+                            name='list'
+                            type='font-awesome'
+                            iconStyle={styles.stackIcon}
+                            onPress={() => navigation.toggleDrawer()}
+                        />
+                    )
+                })}
             ></Stack.Screen>
             <Stack.Screen
                 name='CampsiteInfo'
@@ -103,5 +151,13 @@ const Main = () => {
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    stackIcon: {
+        marginLeft: 10,
+        color: '#fff',
+        fontSize: 24
+    }
+})
 
 export default Main

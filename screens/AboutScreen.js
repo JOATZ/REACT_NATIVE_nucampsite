981a107eb8902fb5a225c0e9a10e70e7847e479b
JOATZ <136692977+JOATZ@users.noreply.github.com>
2023-12-04 11:17:14 +0200

    Animatable Added to pages

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -3,6 +3,7 @@ import { Avatar, Card, ListItem } from 'react-native-elements'
 import { useSelector } from 'react-redux'
 import { baseUrl } from '../shared/baseUrl'
 import Loading from '../components/LoadingComponent'
+import * as Animatable from 'react-native-animatable'
 
 const Mission = () => {
     return (
@@ -41,17 +42,23 @@ const AboutScreen = () => {
     if (partners.errMess) {
         return (
             <ScrollView>
-                <Mission />
-                <Card>
-                    <Card.Title>Community Partners</Card.Title>
-                    <Card.Divider />
-                    <Text>{partners.errMess}</Text>
-                </Card>
+                <Animatable.View
+                    animation='fadeInDown'
+                    duration={2000}
+                    delay={1000}
+                >
+                    <Mission />
+                    <Card>
+                        <Card.Title>Community Partners</Card.Title>
+                        <Card.Divider />
+                        <Text>{partners.errMess}</Text>
+                    </Card>
+                </Animatable.View>
             </ScrollView>
         )
     }
     return (
-        <ScrollView>
+        <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
             <Mission />
             <Card>
                 <Card.Title>Community Partners</Card.Title>
@@ -71,7 +78,7 @@ const AboutScreen = () => {
                     </ListItem>
                 ))}
             </Card>
-        </ScrollView>
+        </Animatable.View>
     )
 }
 
